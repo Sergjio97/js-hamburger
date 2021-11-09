aa04a4a1aef094aa4ba1bd93b64be9e79c287812
Sergjio97 <sergiocizmja@gmail.com>
2021-11-09 15:06:47 +0100

    disattiva hamburger-menu

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ let menu = document.querySelector(".fa-bars");
 menu.addEventListener("click", function(){
     document.querySelector(".hamburger-menu").classList.add("active")
 });
+
+let canc = document.querySelector(".fa-times");
+
+canc.addEventListener("click", function(){
+    document.querySelector(".hamburger-menu").classList.remove("active");
+});
